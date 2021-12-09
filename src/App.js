4b49594e5ac224ff797712e643209c8974003154
Dalvin <dalvin.josias@gmail.com>
2021-12-09 02:00:38 -0800

    add base

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import twitterLogo from './assets/twitter-logo.svg';
-import mvc2Logo from './assets/mvc2_logo.png';
-import './App.css';
-import {TEST_DATA} from './assets/shared/const';
-import React, { useEffect, useState } from 'react';
+import twitterLogo from "./assets/twitter-logo.svg";
+import mvc2Logo from "./assets/mvc2_logo.png";
+import "./App.css";
+import { TEST_DATA } from "./assets/shared/const";
+import React, { useEffect, useState } from "react";
 
 // Constants
-const TWITTER_HANDLE = '_buildspace';
+const TWITTER_HANDLE = "_buildspace";
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const App = () => {
   const [walletAddress, setAddress] = useState(null);
+  const [inputValue, setInputValue] = useState("");
+  const [characterList, setCharacterList] = useState([]);
 
   const renderNotConnectedContainer = () => {
-    return(<button
-      className="cta-button connect-wallet-button"
-      onClick={connectWallet}>
-          Connect Wallet
-      </button>)
-  }
+    return (
+      <button
+        className="cta-button connect-wallet-button"
+        onClick={connectWallet}
+      >
+        Connect Wallet
+      </button>
+    );
+  };
 
   const connectWallet = async () => {
     const { solana } = window;
@@ -27,59 +32,100 @@ const App = () => {
       console.log("Connected with Public Key:", response);
       setAddress(response.publicKey.toString());
     }
+  };
+
+  const onInputChange = (event) => {
+    const { value } = event.target;
+    setInputValue(value);
+  }
+
+  const sendGif = async () => {
+    if (inputValue.length > 0 ) {
+      setCharacterList([...characterList, inputValue]);
+      console.log('hero name', inputValue);
+    } else {
+      console.log("Empty input. Try again");
+    }
   }
 
   const renderConnectedContainer = () => {
-    return(
+    return (
       <div className="connected-container">
-      <div className="gif-grid">
-        {TEST_DATA.map(image => (
-          <div className="gif-item" key={image} >
-            <img src={image} alt={image} width="100" height="100" />
-          </div>
-        ))}
+        <form
+          onSubmit={(event) => {
+            event.preventDefault();
+            sendGif();
+          }}
+        >
+          <input
+            type="text"
+            placeholder="Search For Your Favorite Hero"
+            style={{ backgroundColor: "white" }}
+            onInputChange={onInputChange}
+            value={inputValue}
+          />
+          <button type="submit" className="cta-button submit-gif-button">
+            Submit
+          </button>
+        </form>
+        <div className="gif-grid">
+          {characterList.map((image) => (
+            <div className="gif-item" key={image}>
+              <img src={image} alt={image} width="100" height="100" />
+            </div>
+          ))}
+        </div>
       </div>
-    </div>
-    )
-  }
+    );
+  };
 
-  const connectButton   = async ()  => {
+  const connectButton = async () => {
     const { solana } = window;
 
     try {
-        if (solana) {
-          if (solana.isPhantom) {
-            console.log('Phantom wallet found!');
+      if (solana) {
+        if (solana.isPhantom) {
+          console.log("Phantom wallet found!");
 
-            const response = await solana.connect();
-            console.log(
-              'Connected with Public Key:', response.publicKey.toString()
-            );
-            setAddress(response.publicKey.toString());
-          } else {
-            alert("Solana wallet not detected. Might want to get a Phantom Wallet")
-          }
+          const response = await solana.connect();
+          console.log(
+            "Connected with Public Key:",
+            response.publicKey.toString()
+          );
+          setAddress(response.publicKey.toString());
+        } else {
+          alert(
+            "Solana wallet not detected. Might want to get a Phantom Wallet"
+          );
         }
-    } catch(error) {
+      }
+    } catch (error) {
       console.log("Can't connect to wallet");
     }
-  }
+  };
 
   useEffect(() => {
     const onLoad = async () => {
       await connectButton();
     };
 
-    window.addEventListener('load', onLoad);
-    return () => window.removeEventListener('load', onLoad);
+    window.addEventListener("load", onLoad);
+    return () => window.removeEventListener("load", onLoad);
   }, []);
 
+  useEffect(() => {
+    if (walletAddress) {
+      console.log(" Fetching Hero");
+
+      setCharacterList(TEST_DATA);
+    }
+  }, [walletAddress]);
 
   return (
     <div className="App">
-      <div className={walletAddress ? 'authed-container' : 'container'} >
+      <div className={walletAddress ? "authed-container" : "container"}>
         <div className="header-container">
-          <img src={mvc2Logo} width="250" height="150"/>
+          <img src={mvc2Logo} width="250" height="150" />
           <p className="header">Hero Head Shots</p>
           <p className="sub-text">
             View your Hero collection in the metaverse ✨
