63af364e5144d95bb1e3af260e973da5d7254dac
Dalvin <dalvin.josias@gmail.com>
2021-12-09 01:27:47 -0800

    add basic styling and flavor

--- src/App.js
+++ src/App.js
@@ -1,19 +1,75 @@
 import twitterLogo from './assets/twitter-logo.svg';
+import mvc2Logo from './assets/mvc2_logo.png';
 import './App.css';
+import React, { useEffect, useState } from 'react';
 
 // Constants
 const TWITTER_HANDLE = '_buildspace';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const App = () => {
+  const [walletAddress, setAddress] = useState(null);
+
+  const renderNotConnectedContainer = () => {
+    <button
+    className="cta-button connect-wallet-button"
+    onClick={connectWallet}>
+        Connect Wallet
+    </button>
+  }
+
+  const connectWallet = async () => {
+    const { solana } = window;
+
+    if (solana) {
+      const response = await solana.connect();
+      console.log("Connected with Public Key:", response);
+      setAddress(response.publicKey.toString());
+    }
+  }
+
+  const connectButton   = async ()  => {
+    const { solana } = window;
+
+    try {
+        if (solana) {
+          if (solana.isPhantom) {
+            console.log('Phantom wallet found!');
+
+            const response = await solana.connect();
+            console.log(
+              'Connected with Public Key:', response.publicKey.toString()
+            );
+            setAddress(response.publicKey.toString());
+          } else {
+            alert("Solana wallet not detected. Might want to get a Phantom Wallet")
+          }
+        }
+    } catch(error) {
+      console.log("Can't connect to wallet");
+    }
+  }
+
+  useEffect(() => {
+    const onLoad = async () => {
+      await connectButton();
+    };
+
+    window.addEventListener('load', onLoad);
+    return () => window.removeEventListener('load', onLoad);
+  }, []);
+
+
   return (
     <div className="App">
-      <div className="container">
+      <div className={walletAddress ? 'authed-container' : 'container'} >
         <div className="header-container">
-          <p className="header">🖼 GIF Portal</p>
+          <img src={mvc2Logo} width="250" height="150"/>
+          <p className="header">Hero Head Shots</p>
           <p className="sub-text">
-            View your GIF collection in the metaverse ✨
+            View your Hero collection in the metaverse ✨
           </p>
+          {!walletAddress && renderNotConnectedContainer()}
         </div>
         <div className="footer-container">
           <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
---
+++ src/assets/shared/const.dart
@@ -0,0 +1,8 @@
+
+
+const TEST_DATA = [
+  'https://vignette.wikia.nocookie.net/marvelvscapcom/images/3/34/MVC2_Blackheart.jpg/revision/latest?cb=20110914071301',
+  'https://vignette.wikia.nocookie.net/marvelvscapcom/images/b/be/MVC2_Cable.jpg/revision/latest?cb=20110914072054',
+  'https://vignette.wikia.nocookie.net/marvelvscapcom/images/2/21/MVC2_Captain_America.jpg/revision/latest?cb=20110914072107',
+  'https://vignette.wikia.nocookie.net/marvelvscapcom/images/3/31/MVC2_Colossus.jpg/revision/latest?cb=20110914072123',
+];
