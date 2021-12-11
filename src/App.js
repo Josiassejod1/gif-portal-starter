4677d73cd3f48234fb537ea6e5524624a59085d0
Dalvin <dalvin.josias@gmail.com>
2021-12-10 20:57:14 -0800

    devnet

--- src/App.js
+++ src/App.js
@@ -3,6 +3,21 @@ import mvc2Logo from "./assets/mvc2_logo.png";
 import "./App.css";
 import { TEST_DATA } from "./assets/shared/const";
 import React, { useEffect, useState } from "react";
+import idl from './idl.json';
+import { Connection, PublicKey, clusterApiUrl } from '@solana/web3.js';
+import { Program, Provider, web3 } from '@project-serum/anchor';
+
+const { SystemProgram, Keypair } = web3;
+
+let baseAccount = Keypair.generate();
+
+const programId = new PublicKey(idl.metadata.address);
+
+const network = clusterApiUrl('devnet');
+
+const opts = {
+  preflightCommitment: "processed"
+}
 
 // Constants
 const TWITTER_HANDLE = "_buildspace";
@@ -12,6 +27,15 @@ const App = () => {
   const [walletAddress, setAddress] = useState(null);
   const [inputValue, setInputValue] = useState("");
   const [characterList, setCharacterList] = useState([]);
+
+  const getProvider = () => {
+    const connection = new Connection(network, opts.preflightCommitment);
+    const provider = new Provider(
+      connection, window.solana, opts.preflightCommitment
+    );
+
+    return provider;
+  }
 
   const renderNotConnectedContainer = () => {
     return (
@@ -49,35 +73,67 @@ const App = () => {
   }
 
   const renderConnectedContainer = () => {
-    return (
-      <div className="connected-container">
-        <form
-          onSubmit={(event) => {
-            event.preventDefault();
-            sendGif();
-          }}
-        >
-          <input
-            type="text"
-            placeholder="Search For Your Favorite Hero"
-            style={{ backgroundColor: "white" }}
-            onInputChange={onInputChange}
-            value={inputValue}
-          />
-          <button type="submit" className="cta-button submit-gif-button">
-            Submit
-          </button>
-        </form>
-        <div className="gif-grid">
-          {characterList.map((image) => (
-            <div className="gif-item" key={image}>
-              <img src={image} alt={image} width="100" height="100" />
-            </div>
-          ))}
+    if (characterList === null) {
+      return(
+        <div className="connected-container">
+          <button className="cta-button submit-gif-button" onClick={createHeroAccount}>
+          Do One-Time Initialization For GIF Program Account
+         </button>
         </div>
-      </div>
-    );
+      )
+      } else {
+        return (
+          <div className="connected-container">
+            <form
+              onSubmit={(event) => {
+                event.preventDefault();
+                sendGif();
+              }}
+            >
+              <input
+                type="text"
+                placeholder="Search For Your Favorite Hero"
+                style={{ backgroundColor: "white" }}
+                onInputChange={onInputChange}
+                value={inputValue}
+              />
+              <button type="submit" className="cta-button submit-gif-button">
+                Submit
+              </button>
+            </form>
+            <div className="gif-grid">
+              {characterList?.map((image) => (
+                <div className="gif-item" key={image}>
+                  <img src={image} alt={image} width="100" height="100" />
+                </div>
+              ))}
+            </div>
+          </div>
+        );
+      }
   };
+
+  const createHeroAccount = async () => {
+    try {
+      const provider = getProvider();
+      const program = new Program(idl, programId, provider);
+      console.log("ping");
+      
+      await program.rpc.startStuffOff({
+        accounts: {
+          baseAccount: baseAccount.publicKey,
+          user: provider.wallet.publicKey,
+          systemProgram: SystemProgram.programId,
+        },
+        signers: [baseAccount]
+      });
+
+      console.log("Created a new BaseAccount w/ address:", baseAccount.pub);
+      await getHeroList();
+    } catch(error) {
+      console.log("Error creating BaseAccount account!", error);
+    }
+  }
 
   const connectButton = async () => {
     const { solana } = window;
@@ -104,6 +160,20 @@ const App = () => {
     }
   };
 
+  const getHeroList = async() => {
+    try {
+      const provider = getProvider();
+      const program = new Program(idl, programId, provider);
+      const account = await program.account.baseAccount;
+
+      console.log("Get the account", account);
+      setCharacterList(account.gifList);
+    } catch(error) {
+      console.log("Error is hero List: ", error);
+      setCharacterList([]);
+    }
+  }
+
   useEffect(() => {
     const onLoad = async () => {
       await connectButton();
@@ -115,9 +185,8 @@ const App = () => {
 
   useEffect(() => {
     if (walletAddress) {
-      console.log(" Fetching Hero");
-
-      setCharacterList(TEST_DATA);
+      console.log("Fetching Heros  ...");
+      getHeroList();
     }
   }, [walletAddress]);
 
