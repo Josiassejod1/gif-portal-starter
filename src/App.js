9b52f7cb23a943eb0a7f0aafbffff527968cd10a
Dalvin <dalvin.josias@gmail.com>
2021-12-11 01:35:01 -0800

    new project

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import twitterLogo from "./assets/twitter-logo.svg";
 import mvc2Logo from "./assets/mvc2_logo.png";
 import "./App.css";
 import { TEST_DATA } from "./assets/shared/const";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import idl from './idl.json';
+import Badge from '@material-ui/core/Badge';
 import { Connection, PublicKey, clusterApiUrl } from '@solana/web3.js';
 import { Program, Provider, web3 } from '@project-serum/anchor';
 import kp from './keypair.json';
+import { min } from "bn.js";
+import { set } from "@project-serum/anchor/dist/cjs/utils/features";
 const { SystemProgram, Keypair } = web3;
+
 
 const arr = Object.values(kp._keypair.secretKey)
 const secret = new Uint8Array(arr)
@@ -28,8 +32,12 @@ const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 const App = () => {
   const [walletAddress, setAddress] = useState(null);
   const [inputValue, setInputValue] = useState("");
-  const [characterList, setCharacterList] = useState([]);
+  const [mintedList, setMintList] = useState([]);
+  const [characterFound, setCharacterFound] = useState(false);
+  const [character, setCharacter] = useState(null);
+  const [hash, setHash] = useState({});
 
+  const inputRef = useRef();
   const getProvider = () => {
     const connection = new Connection(network, opts.preflightCommitment);
     const provider = new Provider(
@@ -60,9 +68,31 @@ const App = () => {
     }
   };
 
-  const onInputChange = (event) => {
+  const onChange = (event) => {
     const { value } = event.target;
+    setCharacter(null);
+    setCharacterFound(false);
     setInputValue(value);
+  }
+
+  const onClick = () => {
+    fetchCharacter();
+  }
+
+  const fetchCharacter = () => {
+    console.log("Searching for characters ...")
+    console.log(inputValue);
+    fetch(`https://secure-hamlet-19722.herokuapp.com/api/v1/characters/${inputValue}`)
+    .then(res => res.json())
+    .then((result) => {
+      if (result.length === 0) {
+        setCharacterFound(false);
+      } else {
+        console.log(result);
+        setCharacter(result[0]);
+        setCharacterFound(true);
+      }
+    });
   }
 
   const sendGif = async () => {
@@ -70,29 +100,49 @@ const App = () => {
       console.log("No gif link given!");
       return
     }
-    setInputValue('');
-    console.log("Gif Link:", inputValue);
+    console.log("Gif Link:", character.head_shot);
 
     try {
       const provider = getProvider();
       const program = new Program(idl, programId, provider);
 
-      await program.rpc.addGif(inputValue, {
+      await program.rpc.addGif(character.head_shot, {
         accounts:{
           baseAccount: baseAccount.publicKey,
           user: provider.wallet.publicKey,
         }
       });
 
-      console.log("GIF successfully sent to program", inputValue)
-      await getGifList();
+      console.log("GIF successfully sent to program", inputValue, character.head_shot)
+      await getHeroList();
     } catch(error) {
       console.log("Error sending GIF:", error);
     }
+
+    setCharacter(null);
+    setInputValue('');
+    setCharacterFound(false);
+    inputRef.current.value = '';
+  }
+
+  const getBadgeCount = (mintedList) => {
+
+    mintedList.forEach((character) => {
+        if (hash[character.gifLink]) {
+         setHash(
+          hash[character.gifLink] += 1
+         )
+        } else {
+         setHash(
+          hash[character.gifLink] = 1
+         )
+        }
+      });
+    console.log(hash);
   }
 
   const renderConnectedContainer = () => {
-    if (characterList === null) {
+    if (mintedList === null) {
       return(
         <div className="connected-container">
           <button className="cta-button submit-gif-button" onClick={createHeroAccount}>
@@ -106,27 +156,52 @@ const App = () => {
             <form
               onSubmit={(event) => {
                 event.preventDefault();
+               if (characterFound) {
                 sendGif();
+               }
               }}
             >
+              <p className="sub-text">Character's names are case sensitive*</p>
+              <p className="sub-text">Full name list <a href="https://secure-hamlet-19722.herokuapp.com/characters" target="_blank">here</a></p>
               <input
+                ref={inputRef}
                 type="text"
-                placeholder="Search For Your Favorite Hero"
-                style={{ backgroundColor: "white" }}
-                onInputChange={onInputChange}
-                value={inputValue}
+                onChange={onChange}
+                style={{ backgroundColor: "gray" }}
               />
-              <button type="submit" className="cta-button submit-gif-button">
-                Submit
+               <button type="submit" className="cta-button search-gif-button" onClick={onClick}>
+                Search
               </button>
+             {
+               characterFound &&  <button type="submit" className="cta-button submit-gif-button">
+               Submit
+             </button>
+             }
             </form>
             <div className="gif-grid">
-              {characterList?.map((image) => (
-                <div className="gif-item" key={image}>
-                  <img src={image} alt={image} width="100" height="100" />
-                </div>
-              ))}
+              {character && <div className="gif-item" key={character.head_shot}>
+                  <img src={character.head_shot} alt={character.name} width="100"/>
+                </div>}
+
+              {character && <p className="sub-text">{character.name} HeadShot Selected</p>}
+              {character && <input type="text"  value={character.head_shot} readonly style={{color: "gray", backgroundColor: "lightgray"}}/>}
             </div>
+            {mintedList?.length > 0  &&  <div>
+                <p className="sub-text">Minted Character List </p>
+                {
+                  mintedList?.map((character) => (
+                    <div   className="gif-grid">
+                       <Badge badgeContent={hash[character.gifLink]} color="primary">
+                      <div className="gif-item" key={character.gifLink}>
+                     
+                      <img src={character.gifLink} alt={character.gifLink} width="100" height="100"/>
+                      <p className="sub-text" style={{fontSize: 12, overflow: "hidden", textOverflow: "ellipsis", width: 100}}> character minted by: {character.userAddress.toString()}</p>
+                    </div>
+                    </Badge>
+                    </div>
+                  ))
+                }
+              </div>}
           </div>
         );
       }
@@ -149,6 +224,7 @@ const App = () => {
 
       console.log("Created a new BaseAccount w/ address:", baseAccount.pub);
       await getHeroList();
+
     } catch(error) {
       console.log("Error creating BaseAccount account!", error);
     }
@@ -183,14 +259,18 @@ const App = () => {
     try {
       const provider = getProvider();
       const program = new Program(idl, programId, provider);
-      const account = await program.account.baseAccount;
+      const account = await program.account.baseAccount.fetch(baseAccount.publicKey);
 
       console.log("Get the account", account);
-      setCharacterList(account.gifList);
+      console.log("gif list", account.gifList);
+      setMintList(account.gifList);
+     // getBadgeCount(account.gifList);
     } catch(error) {
       console.log("Error is hero List: ", error);
-      setCharacterList([]);
+      setMintList([]);
     }
+
+    
   }
 
   useEffect(() => {
